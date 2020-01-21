442332c98291f3ae57f331c37d338038f08cd795
Piscki Pratama <pisckipratama@gmail.com>
2020-01-21 17:32:48 +0700

    revisi sql

--- challange14/query_university.sql
+++ challange14/query_university.sql
@@ -23,8 +23,8 @@ create table jurusan(
 );
 
 create table kontrak(
-    id_kontrak integer primary key not null,
-    nilai numeric(10),
+    id_kontrak integer primary key,
+    nilai char(2),
     nim integer not null,
     id_matkul integer not null,
     id_dosen integer not null,
@@ -34,50 +34,39 @@ create table kontrak(
 );
 
 insert into jurusan (id_jurusan, nama_jurusan) values(1001, 'Teknik Informatika');
-insert into jurusan (id_jurusan, nama_jurusan) values(1002, 'Statistika');
-insert into jurusan (id_jurusan, nama_jurusan) values(1003, 'Biologi');
-insert into jurusan (id_jurusan, nama_jurusan) values(1004, 'Teknologi Pangan');
-insert into jurusan (id_jurusan, nama_jurusan) values(1005, 'Sistem Informasi');
+insert into jurusan (id_jurusan, nama_jurusan) values(1002, 'Teknik Komputer');
+insert into jurusan (id_jurusan, nama_jurusan) values(1003, 'Sistem Informasi');
 
-insert into mahasiswa (nim, nama_mahasiswa, alamat, id_jurusan) values(20101, 'Kepa Arizabalaga', 'London Kidul', '1005');
-insert into mahasiswa (nim, nama_mahasiswa, alamat, id_jurusan) values(20102, 'Antonio Rudiger', 'London Wetan', '1002');
-insert into mahasiswa (nim, nama_mahasiswa, alamat, id_jurusan) values(20103, 'Andreas Christensen', 'London Kaler', '1003');
-insert into mahasiswa (nim, nama_mahasiswa, alamat, id_jurusan) values(20104, 'Cesar Azpilicueta', 'London Kulon', '1004');
-insert into mahasiswa (nim, nama_mahasiswa, alamat, id_jurusan) values(20105, 'Ngolo Kante', 'London Lama', '1001');
-insert into mahasiswa (nim, nama_mahasiswa, alamat, id_jurusan) values(20106, 'Reece James', 'London Tengah', '1002');
-insert into mahasiswa (nim, nama_mahasiswa, alamat, id_jurusan) values(20107, 'Jorginho', 'London Pinggir', '1003');
-insert into mahasiswa (nim, nama_mahasiswa, alamat, id_jurusan) values(20108, 'Mason Mount', 'London Pesisir', '1004');
-insert into mahasiswa (nim, nama_mahasiswa, alamat, id_jurusan) values(20109, 'Hudson Odoi', 'London Tengah', '1005');
-insert into mahasiswa (nim, nama_mahasiswa, alamat, id_jurusan) values(20110, 'Tammy Abraham', 'London Wetan', '1001');
-insert into mahasiswa (nim, nama_mahasiswa, alamat, id_jurusan) values(20110, 'Willian', 'London Wetan', '1002');
+insert into mahasiswa (nim, nama_mahasiswa, alamat, id_jurusan) values(20101, 'Kasih Adinda', 'Jambi', '1001');
+insert into mahasiswa (nim, nama_mahasiswa, alamat, id_jurusan) values(20102, 'Fenti Rahmawati', 'Ciamis', '1003');
+insert into mahasiswa (nim, nama_mahasiswa, alamat, id_jurusan) values(20103, 'Kulsum Sopiani', 'Bandung', '1003');
+insert into mahasiswa (nim, nama_mahasiswa, alamat, id_jurusan) values(20114, 'Arip Rahman', 'Garut', '1002');
+insert into mahasiswa (nim, nama_mahasiswa, alamat, id_jurusan) values(20115, 'Piscki Pratama', 'Jakarta', '1001');
 
-insert into mata_kuliah (id_matkul, nama_matkul, sks) values (1, "Aljabar Linier Elementer", 3);
-insert into mata_kuliah (id_matkul, nama_matkul, sks) values (2, "Sistem Informasi Manajemen", 4);
-insert into mata_kuliah (id_matkul, nama_matkul, sks) values (3, "Manajemen", 3);
-insert into mata_kuliah (id_matkul, nama_matkul, sks) values (4, "Algoritma dan Pemrograman", 4);
-insert into mata_kuliah (id_matkul, nama_matkul, sks) values (5, "Pengantar Bisnis", 2);
-insert into mata_kuliah (id_matkul, nama_matkul, sks) values (6, "Kalkulus", 4);
-insert into mata_kuliah (id_matkul, nama_matkul, sks) values (7, "Matematika Dasar", 3);
-insert into mata_kuliah (id_matkul, nama_matkul, sks) values (8, "Pengantar Informatika", 3);
-insert into mata_kuliah (id_matkul, nama_matkul, sks) values (9, "Pendidikan Agama", 4);
-insert into mata_kuliah (id_matkul, nama_matkul, sks) values (10, "Pendidikan Pancasila", 3);
-insert into mata_kuliah (id_matkul, nama_matkul, sks) values (11, "Pengantar Biokimia", 3);
-insert into mata_kuliah (id_matkul, nama_matkul, sks) values (12, "Sepakbola untuk Profesional", 2);
+insert into mata_kuliah (id_matkul, nama_matkul, sks) values (11, "Sistem Informasi Manajemen", 4);
+insert into mata_kuliah (id_matkul, nama_matkul, sks) values (12, "Manajemen", 3);
+insert into mata_kuliah (id_matkul, nama_matkul, sks) values (13, "Algoritma dan Pemrograman", 4);
+insert into mata_kuliah (id_matkul, nama_matkul, sks) values (14, "Pengantar Bisnis", 2);
+insert into mata_kuliah (id_matkul, nama_matkul, sks) values (15, "Kalkulus", 4);
+insert into mata_kuliah (id_matkul, nama_matkul, sks) values (16, "Matematika Dasar", 3);
+insert into mata_kuliah (id_matkul, nama_matkul, sks) values (17, "Pengantar Informatika", 3);
+insert into mata_kuliah (id_matkul, nama_matkul, sks) values (18, "Pendidikan Agama", 4);
+insert into mata_kuliah (id_matkul, nama_matkul, sks) values (19, "Pendidikan Pancasila", 3);
+insert into mata_kuliah (id_matkul, nama_matkul, sks) values (20, "Aljabar Linier Elementer", 3);
 
-insert into dosen (id_dosen, nama_dosen) values (101, "Asep Smith Sinaga");
-insert into dosen (id_dosen, nama_dosen) values (102, "Umuh Muchtar");
-insert into dosen (id_dosen, nama_dosen) values (103, "Upin Danipin");
-insert into dosen (id_dosen, nama_dosen) values (104, "Frank Lampard");
-insert into dosen (id_dosen, nama_dosen) values (105, "Rojali Siregar");
+insert into dosen (id_dosen, nama_dosen) values (101, "John Doe");
+insert into dosen (id_dosen, nama_dosen) values (102, "Chalie Atson");
+insert into dosen (id_dosen, nama_dosen) values (103, "Gerald Alberts");
+insert into dosen (id_dosen, nama_dosen) values (104, "Frankie Smith");
+insert into dosen (id_dosen, nama_dosen) values (105, "Stephany Angela");
 
-insert into kontrak (id_kontrak, nim, id_matkul, id_dosen, nilai) values (311, 20101, 01, 101, 81);
-insert into kontrak (id_kontrak, nim, id_matkul, id_dosen, nilai) values (312, 20102, 02, 101, 85);
-insert into kontrak (id_kontrak, nim, id_matkul, id_dosen, nilai) values (313, 20103, 03, 102, 80);
-insert into kontrak (id_kontrak, nim, id_matkul, id_dosen, nilai) values (314, 20104, 04, 102, 76);
-insert into kontrak (id_kontrak, nim, id_matkul, id_dosen, nilai) values (315, 20105, 05, 103, 72);
-insert into kontrak (id_kontrak, nim, id_matkul, id_dosen, nilai) values (316, 20106, 06, 103, 70);
-insert into kontrak (id_kontrak, nim, id_matkul, id_dosen, nilai) values (317, 20107, 07, 104, 69);
-insert into kontrak (id_kontrak, nim, id_matkul, id_dosen, nilai) values (318, 20108, 08, 104, 87);
-insert into kontrak (id_kontrak, nim, id_matkul, id_dosen, nilai) values (319, 20109, 09, 105, 93);
-insert into kontrak (id_kontrak, nim, id_matkul, id_dosen, nilai) values (320, 20110, 10, 105, 78);
-insert into kontrak (id_kontrak, nim, id_matkul, id_dosen, nilai) values (321, 20111, 12, 104, 70);
+insert into kontrak (nim, id_matkul, id_dosen, nilai) values (20101, 11, 101, 'A');
+insert into kontrak (nim, id_matkul, id_dosen, nilai) values (20102, 12, 101, 'A');
+insert into kontrak (nim, id_matkul, id_dosen, nilai) values (20103, 13, 102, 'B');
+insert into kontrak (nim, id_matkul, id_dosen, nilai) values (20104, 14, 102, 'B');
+insert into kontrak (nim, id_matkul, id_dosen, nilai) values (20105, 15, 103, 'C');
+insert into kontrak (nim, id_matkul, id_dosen, nilai) values (20101, 16, 103, 'C');
+insert into kontrak (nim, id_matkul, id_dosen, nilai) values (20102, 17, 104, 'D');
+insert into kontrak (nim, id_matkul, id_dosen, nilai) values (20103, 18, 104, 'D');
+insert into kontrak (nim, id_matkul, id_dosen, nilai) values (20104, 19, 105, 'E');
+insert into kontrak (nim, id_matkul, id_dosen, nilai) values (20105, 20, 105, 'E');
